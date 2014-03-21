bfd51c8477e9abf10ef922f0c9f8e05d3897859a
Felipe Rodrigues <lfrs.web@gmail.com>
2014-03-21 03:26:19 +0000

    Add bootstrap loading state when user submit nickname's Fix idententaion

--- public/chat.js
+++ public/chat.js
@@ -116,105 +116,114 @@ G)});
 
 
 var $messageInput
-  , $chatContainer
-  , $nickname
-  , $goChat
-  , userNickname = ''
-  , $scroll;
+    , $chatContainer
+    , $nickname
+    , $goChat
+    , userNickname = ''
+    , $scroll;
 
 $(function() {
-  $nickname = $('#nickname');
-  $messageInput = $('#message');
-  $chatContainer = $('#chat-container');
-  $goChat = $('#go-chat');
+    $nickname = $('#nickname');
+    $messageInput = $('#message');
+    $chatContainer = $('#chat-container');
+    $goChat = $('#go-chat');
 
-  $scroll = $chatContainer.niceScroll({
-    cursorwidth: 8
-  });
+    $scroll = $chatContainer.niceScroll({
+        cursorwidth: 8
+    });
 
-  $goChat.on('click', setNickname);
+    $goChat.on('click', setNickname);
 
-  $('#submit').on('click', sendMessage);
-  $('form').on('submit', function(e) {
-    e.preventDefault();
-    return false;
-  });
-  $('.form-chat').on('submit', function(e) {
-    e.preventDefault();
-    sendMessage();
-    return false;
-  });
+    $('#submit').on('click', sendMessage);
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        return false;
+    });
+    $('.form-chat').on('submit', function(e) {
+        e.preventDefault();
+        sendMessage();
+        return false;
+    });
 });
 
 
 // Socket.io
 var socket = io.connect();
 socket.on('connect', function() {
-  console.log('Connected');
+    console.log('Connected');
 });
 
 socket.on('message', function(data) {
-  addMessage(data['message'], data['nickname'], data['date'], false);
+    addMessage(data['message'], data['nickname'], data['date'], false);
 });
 
 socket.on('online-users', function(data) {
-  $('.online-users').html(data.join(', '));
+    $('.online-users').html(data.join(', '));
 });
 
 
 // Helper functions
 function sendMessage() {
-  if (userNickname == '') {
-    alert('Who are you?');
-    $nickname.focus();
+    if (userNickname == '') {
+        alert('Who are you?');
+        $nickname.focus();
 
-    return false;
-  }
+        return false;
+    }
 
-  var message = $messageInput.val();
-  if (message != '') {
-    socket.emit('message', message);
-    addMessage(message, 'Me', new Date().toISOString(), true)
-    $messageInput.val('');
-  }
+    var message = $messageInput.val();
+    if (message != '') {
+        socket.emit('message', message);
+        addMessage(message, 'Me', new Date().toISOString(), true)
+        $messageInput.val('');
+    }
 }
 
 function addMessage(msg, nick, date, isSelf) {
-  var pClasses = 'message';
-  var message = nick + ': ' + msg;
+    var pClasses = 'message';
+    var message = nick + ': ' + msg;
 
-  if (isSelf) {
-    pClasses += ' me';
-    message = msg;
-  }
+    if (isSelf) {
+        pClasses += ' me';
+        message = msg;
+    }
 
-  $chatContainer.append('<div class="row"><p class="'+ pClasses+'">' + message + '</p></div>');
-  $messageInput.focus();
+    $chatContainer.append('<div class="row"><p class="'+ pClasses+'">' + message + '</p></div>');
+    $messageInput.focus();
 
-  // Scroll to the last message
-  setTimeout(function() {
-    var $lastMessage= $chatContainer.find('.row:last');
-    var offsetTop = $lastMessage.offset().top + $lastMessage.height();
+    // Scroll to the last message
+    setTimeout(function() {
+        var $lastMessage= $chatContainer.find('.row:last');
+        var offsetTop = $lastMessage.offset().top + $lastMessage.height();
 
-    $scroll.doScrollTo(offsetTop);
-  }, 300);
+        $scroll.doScrollTo(offsetTop);
+    }, 300);
 
 }
 
 function setNickname() {
-  userNickname = $nickname.val();
-  if (userNickname) {
-    socket.emit('setNickname', userNickname);
+    var btn = $(this);
+    btn.button('loading');
 
-    socket.on('nickname', function(data) {
-      if (data == 'ok') {
-        $('#form-nickname').hide();
-        $('#chat').show();
-        $messageInput.focus();
-      } else {
-        userNickname = '';
-        alert('This nickname is already taken.');
-      }
-    });
-  }
+    userNickname = $nickname.val();
+
+    if (userNickname) {
+        socket.emit('setNickname', userNickname);
+
+        socket.on('nickname', function(data) {
+            if (data == 'ok') {
+                $('#form-nickname').hide();
+                $('#chat').show();
+                $messageInput.focus();
+
+                btn.button('reset');
+            } else {
+                userNickname = '';
+                alert('This nickname is already taken.');
+
+                btn.button('reset');
+            }
+        });
+    }
+
 }
--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 // Start libraries and server
 var express = require('express')
-  , app = express();
+    , app = express();
 
 var http = require('http')
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server);
+    , server = http.createServer(app)
+    , io = require('socket.io').listen(server);
 
 
 // app config
 app.configure(function() {
-  // Views directory
-  app.set('views', __dirname + '/views');
-  // Static files directory
-  app.use(express.static(__dirname + '/public'));
-  // Disable layout
-  app.set("view options", {layout: false});
-  // Views engine
-  app.set('view engine', 'jade');
+    // Views directory
+    app.set('views', __dirname + '/views');
+    // Static files directory
+    app.use(express.static(__dirname + '/public'));
+    // Disable layout
+    app.set("view options", {layout: false});
+    // Views engine
+    app.set('view engine', 'jade');
 });
 
 
 // Render the main page
 app.get('/', function(req, res) {
-  res.render('chat.jade');
+    res.render('chat.jade');
 });
 
 
@@ -32,76 +32,76 @@ var nicknames = [];
 // Connection event
 io.sockets.on('connection', function(socket) {
 
-  // Broadcast the message to all
-  socket.on('message', function(data) {
-    var userNickname = getNickname(socket);
+    // Broadcast the message to all
+    socket.on('message', function(data) {
+        var userNickname = getNickname(socket);
 
-    if (userNickname) {
-      var transmit = {
-        date: new Date().toISOString(),
-        nickname: userNickname,
-        message: data
-      };
+        if (userNickname) {
+            var transmit = {
+                date: new Date().toISOString(),
+                nickname: userNickname,
+                message: data
+            };
 
-      socket.broadcast.emit('message', transmit);
-      console.log('User %s said "%s"', transmit.nickname, data);
-    }
-  });
+            socket.broadcast.emit('message', transmit);
+            console.log('User %s said "%s"', transmit.nickname, data);
+        }
+    });
 
-  // Set user nickname
-  socket.on('setNickname', function(data) {
-    // Test if nickname is already taken
-    if (nicknames.indexOf(data) == -1) {
-      socket.set('nickname', data, function() {
-        nicknames.push(data);
-        socket.emit('nickname', 'ok');
+    // Set user nickname
+    socket.on('setNickname', function(data) {
+        // Test if nickname is already taken
+        if (nicknames.indexOf(data) == -1) {
+            socket.set('nickname', data, function() {
+                nicknames.push(data);
+                socket.emit('nickname', 'ok');
 
-        emitOnlineUsers();
-        console.log('User %s connected.', data);
-      });
-    } else {
-      // Send the error
-      socket.emit('nickname', 'error');
-    }
+                emitOnlineUsers();
+                console.log('User %s connected.', data);
+            });
+        } else {
+            // Send the error
+            socket.emit('nickname', 'error');
+        }
 
-    console.log(nicknames);
-  });
+        console.log(nicknames);
+    });
 
-  // Disconnection of the client
-  // Remove the user's nickname
-  socket.on('disconnect', function() {
-    var userNickname = getNickname(socket);
+    // Disconnection of the client
+    // Remove the user's nickname
+    socket.on('disconnect', function() {
+        var userNickname = getNickname(socket);
 
-    if (userNickname) {
-      var index = nicknames.indexOf(userNickname);
-      nicknames.splice(index, 1);
+        if (userNickname) {
+            var index = nicknames.indexOf(userNickname);
+            nicknames.splice(index, 1);
 
-      emitOnlineUsers();
-      console.log('User %s has been disconnected', userNickname);
-    }
+            emitOnlineUsers();
+            console.log('User %s has been disconnected', userNickname);
+        }
 
-  });
+    });
 
 });
 
 /**
- * Get the user nickname
- */
+* Get the user nickname
+*/
 function getNickname(socket) {
-  var nickname = false;
+    var nickname = false;
 
-  socket.get('nickname', function(err, name) {
-    if (name != null) {
-      nickname = name;
-    }
-  });
+    socket.get('nickname', function(err, name) {
+        if (name != null) {
+            nickname = name;
+        }
+    });
 
-  return nickname;
+    return nickname;
 }
 
 /**
- * Emmit the 'online-users' evento to all clients
- */
+* Emmit the 'online-users' evento to all clients
+*/
 function emitOnlineUsers() {
     io.sockets.emit('online-users', nicknames);
 }
@@ -111,5 +111,5 @@ function emitOnlineUsers() {
 var port = Number(process.env.PORT || 5000);
 
 server.listen(port, function() {
-  console.log('Listening on port %d', server.address().port);
+    console.log('Listening on port %d', server.address().port);
 });
