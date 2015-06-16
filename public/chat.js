4cb0260d5bbb35a9dbec8122255719b1952bbbbd
Jorge Cristina <jcristin@sapo.pt>
2015-06-16 22:35:28 +0100

    Version 1.0

--- public/chat.js
+++ public/chat.js
@@ -38,11 +38,7 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(data) {
-    addMessage(data['message'], data['nickname'], data['date'], false);
-});
-
-socket.on('online-users', function(data) {
-    $('.online-users').html(data.join(', '));
+    addMessage(data['message'], data['nickname'], data['date'], data['sender']);
 });
 
 
@@ -51,35 +47,40 @@ function sendMessage() {
     if (userNickname == '') {
         alert('Who are you?');
         $nickname.focus();
-
         return false;
     }
 
     var message = $messageInput.val();
     if (message != '') {
         socket.emit('message', message);
-        addMessage(message, 'Me', new Date().toISOString(), true)
+        addMessage(message, 'Me', new Date().toISOString(), 2)
         $messageInput.val('');
     }
 }
 
-function addMessage(msg, nick, date, isSelf) {
+function addMessage(msg, nick, date, sender) {
     var pClasses = 'message';
-    var message = nick + ': ' + msg;
+    var message;
 
-    if (isSelf) {
-        pClasses += ' me';
-        message = msg;
+    if (sender === 3) {
+        pClasses += ' server';
+        message = 'Server: ' + msg;
     }
+    else {
+        if (sender === 2) {
+            pClasses += ' me';
+            message = msg;
+        }
+        else {
+            var message = nick + ': ' + msg;
+        }
+    } 
 
     $chatContainer.append('<div class="row"><p class="'+ pClasses+'">' + _.escape(message) + '</p></div>');
     $messageInput.focus();
 
     // Scroll to the last message
     setTimeout(function() {
-        //var $lastMessage= $chatContainer.find('.row:last');
-        //var offsetTop = $lastMessage.offset().top + $lastMessage.height();
-
         $scroll.doScrollTo(999999);
     }, 300);
 
@@ -92,22 +93,46 @@ function setNickname() {
     userNickname = $nickname.val();
 
     if (userNickname) {
-        socket.emit('setNickname', userNickname);
-
-        socket.on('nickname', function(data) {
-            if (data == 'ok') {
-                $('#form-nickname').hide();
-                $('#chat').show();
-                $messageInput.focus();
-
-                btn.button('reset');
-            } else {
-                userNickname = '';
-                alert('This nickname is already taken.');
-
-                btn.button('reset');
-            }
+        
+        socket.emit('nick', userNickname);
+        
+        socket.on('badnick', function () {
+            userNickname = '';
+            alert('This nickname is already taken.');
+            btn.button('reset');
         });
-    }
+        
+        socket.on('logon', function (data) {
+            $('#form-nickname').hide();
+            $('#chat').show();
+            $messageInput.focus();
+            addMessage('Entrou no chat como ' + data + '.',"", "", 3);
+            btn.button('reset');
+        });
+        
+        socket.on('welcome', function (agent) {
+            $('#form-nickname').hide();
+            $('#chat').show();
+            $messageInput.focus();
+            btn.button('reset');
+            var day = new Date();
+            var hr = day.getHours();
+            var welcomeMsg = "Bom dia";
+            if (hr >= 12) {
+                if (hr < 20) {
+                    welcomeMsg = "Boa tarde";
+                }
+                else {
+                    welcomeMsg = "Boa noite";
+                }
+            }
+            socket.emit('info', 'Cliente '+userNickname+' a iniciar conversa\347\343o.');
+            addMessage(welcomeMsg + ', o meu nome \351 ' + agent + ', em que posso ajudar?', agent, "", 1);
+        });
 
+        socket.on('noagents', function (data) {
+            alert("De momento n\343o h\341 agentes dispon\355veis. Por favor, tente mais tarde ou ligue 21 112 7303.");
+            btn.button('reset');
+        });     
+    }
 }
--- server.js
+++ server.js
@@ -25,87 +25,127 @@ app.get('/', function(req, res) {
     res.render('chat.jade');
 });
 
-
 // Users nickname's
 var nicknames = [];
+// Rooms & agents
+var rooms = ['dhl1', 'dhl2', 'dhl3', 'dhl4'];
+var agents = ['Jo\343o', 'Duarte', 'Teresa', 'Miguel'];
 
 // Connection event
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
+    
+    // Once we receive a nickname we connect it to next free room    
+    socket.once('nick', function (data) { connect(socket, data); });
 
     // Broadcast the message to all
     socket.on('message', function(data) {
         var userNickname = getNickname(socket);
-
         if (userNickname) {
             var transmit = {
                 date: new Date().toISOString(),
                 nickname: userNickname,
-                message: data
+                message: data,
+                sender: 1
             };
-
-            socket.broadcast.emit('message', transmit);
-            console.log('User %s said "%s"', transmit.nickname, data);
+            socket.broadcast.to(socket.room).emit('message', transmit);
+            console.log('User %s said "%s" at %s.', transmit.nickname, data, transmit.date);
         }
     });
-
-    // Set user nickname
-    socket.on('setNickname', function(data) {
-        // Test if nickname is already taken
-        if (nicknames.indexOf(data) == -1) {
-            socket.set('nickname', data, function() {
-                nicknames.push(data);
-                socket.emit('nickname', 'ok');
-
-                emitOnlineUsers();
-                console.log('User %s connected.', data);
-            });
-        } else {
-            // Send the error
-            socket.emit('nickname', 'error');
+    
+    // Broadcast the message to all
+    socket.on('info', function (data) {
+        var userNickname = getNickname(socket);
+        if (userNickname) {
+            var transmit = {
+                date: new Date().toISOString(),
+                nickname: userNickname,
+                message: data,
+                sender: 3
+            };
+            socket.broadcast.to(socket.room).emit('message', transmit);
+            console.log('User %s said "%s" at %s.', transmit.nickname, data, transmit.date);
         }
-
-        console.log(nicknames);
     });
 
     // Disconnection of the client
     // Remove the user's nickname
     socket.on('disconnect', function() {
         var userNickname = getNickname(socket);
-
         if (userNickname) {
             var index = nicknames.indexOf(userNickname);
             nicknames.splice(index, 1);
-
-            emitOnlineUsers();
+            var transmit = {
+                date: new Date().toISOString(),
+                nickname: userNickname,
+                message: userNickname +' acabou de desligar o chat',
+                sender: 3
+            }
+            socket.broadcast.to(socket.room).emit('message', transmit);
             console.log('User %s has been disconnected', userNickname);
         }
-
     });
 
 });
+
+function connect(socket, nickname) {
+    
+    console.log("Current users are %s.",nicknames);
+
+    // if agent then subscribe to its room
+    var i = rooms.indexOf(nickname);
+    if ( i > -1 ) {
+        socket.join(nickname);
+        socket.set('nickname', nickname, function () {
+            nicknames.push(nickname);
+        });
+        socket.room = rooms[i];
+        socket.emit('logon', agents[i]);
+        console.log('Agent %s connected.', agents[i]);
+    }
+    // if not agent then find the first empty room and subscribe to it
+    else {
+        
+        if (nicknames.indexOf(nickname) == -1) {
+            console.log('User %s connected.', nickname);
+            for (var i in rooms) {
+                var socketIds = io.sockets.manager.rooms["/"+rooms[i]];
+                if (socketIds && socketIds.length === 1) {
+                    socket.join(rooms[i]);
+                    socket.room = rooms[i];
+                    socket.set('nickname', nickname, function () { nicknames.push(nickname); });
+                    socket.emit('welcome', agents[i]);
+                    console.log('User %s connected to room %s.', nickname, rooms[i]);
+                    return;
+                }
+            }
+            socket.emit('noagents');
+            socket.leave(socket);           
+        } 
+        else {
+            // Send the error
+            socket.emit('badnick');
+        }
+    }
+}
+
 
 /**
 * Get the user nickname
 */
 function getNickname(socket) {
     var nickname = false;
-
+    
     socket.get('nickname', function(err, name) {
         if (name != null) {
             nickname = name;
         }
     });
-
+    var i = rooms.indexOf(nickname);
+    if (i > -1) {
+        nickname = agents[i];
+    }
     return nickname;
 }
-
-/**
-* Emmit the 'online-users' evento to all clients
-*/
-function emitOnlineUsers() {
-    io.sockets.emit('online-users', nicknames);
-}
-
 
 // Starts the server at port 3000
 var port = Number(process.env.PORT || 3000);
